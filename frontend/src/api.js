857600dbd853ab7b1970e336148c604468270b1b
bytebyabhay-abhx <abhaysarwade69@gmail.com>
2025-11-19 20:41:05 +0530

    api.js updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,24 +1,34 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-})
+    baseURL: import.meta.env.VITE_API_URL,
+});
 
 api.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem(ACCESS_TOKEN);
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`
+
+        // ❗ Do NOT send token for login or register
+        const publicEndpoints = [
+            "/api/user/register/",
+            "/api/token/",
+            "/api/token/refresh/",
+        ];
+
+        const isPublic = publicEndpoints.some((url) =>
+            config.url.includes(url)
+        );
+
+        if (!isPublic && token) {
+            config.headers.Authorization = `Bearer ${token}`;
         }
-        return config
+
+        return config;
     },
     (error) => {
-        return Promise.reject(error)
+        return Promise.reject(error);
     }
+);
 
-
-)
-
-export default api
+export default api;
